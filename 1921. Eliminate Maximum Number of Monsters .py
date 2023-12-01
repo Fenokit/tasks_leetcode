ab46bef5b223423dd92b86b7322289d4cf6e2944
Renat Mukanov <103631832+Fenokit@users.noreply.github.com>
2023-12-01 14:19:49 +0400

    Updated task 1921.

--- 1921. Eliminate Maximum Number of Monsters .py
+++ 1921. Eliminate Maximum Number of Monsters .py
@@ -6,24 +6,12 @@ class Solution:
         Finds the maximum number of monsters killed.
         """
         total_steps = [el / speed[index_el] for index_el, el in enumerate(dist)]
-        total_steps.sort(reverse=True)
-        num = 0
-        while True:
-            if not total_steps:
-                return num if num > 0 else 1
-            for x in total_steps:
-                if x <= 0:
-                    return num if num > 0 else 1
-            x = 1
-            if len(total_steps) > 1000 and total_steps[-1] > 1000:
-                x = 1000
-            elif len(total_steps) > 100 and total_steps[-1] > 100:
-                x = 100
-            elif len(total_steps) > 10 and total_steps[-1] > 10:
-                x = 10
+        total_steps.sort()
 
-            num += x
-            total_steps = [y - x for y in total_steps[:-x]]
+        for index, el in enumerate(total_steps):
+            if index >= el:
+                return index
+        return len(total_steps)
 
 
 if __name__ == '__main__':
